751484b1b758704c7411b040b0a5a7ef4d22ad9f
Stephen McMullin <stephen.mcmullin@icloud.com>
2020-08-12 21:15:08 -0400

    Added content box content

--- src/ShowArticles.js
+++ src/ShowArticles.js
@@ -4,14 +4,18 @@ const ShowArticles = (props) => {
     return(
         props.articles.map( (article, index) => {
             return (
-                <div key={index} className="articleContainer wrapper">
+                <div key={index} className="articleContainer">
                 
                     <div>
                         <img src={article.urlToImage} alt="" />
                     </div>
-                    <div>
+                    <div className="contentBox">
 
-                        <a href={article.url}><p>{article.title}</p></a>
+                        <a href={article.url}><h2>{article.title}</h2></a>
+                        <datetime>{article.publishedAt}</datetime>
+                        <p>{article.description}</p>
+                        <a href={article.url}>Read more...</a>
+                        <p>Source: {article.source.name}</p>
 
                     </div>
                 </div>
