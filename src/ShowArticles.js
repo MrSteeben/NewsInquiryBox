ae038ea3d45e8f996f072c5666bf77582b482621
Stephen McMullin <stephen.mcmullin@icloud.com>
2023-10-12 13:25:53 -0400

    ShowArticles a link updates

--- src/ShowArticles.js
+++ src/ShowArticles.js
@@ -4,7 +4,7 @@ const ShowArticles = (props) => {
     return(
         props.articles.map( (article, index) => {
             return (
-                <a href={article.url} className="articleBoxEffect" target="_blank">
+                <a href={article.url} className="articleBoxEffect" target="_blank" rel="noreferrer">
                     <div key={index} className="articleContainer">
                 
                         <div>
@@ -12,12 +12,12 @@ const ShowArticles = (props) => {
                         </div>
                         <div className="contentBox">
 
-                            <a href={article.url} target="_blank"><h2>{article.title}</h2></a>
+                            <a href={article.url} target="_blank" rel="noreferrer"><h2>{article.title}</h2></a>
                             <h3>Author: {article.author}</h3>
                             <p className="publishedTime">{article.publishedAt.slice(0, 10)}</p>
                             <p className="newsSource">Source: {article.source.name}</p>
                             <p className="articleDescription">{article.description}</p>
-                            <a href={article.url} target="_blank">Read more...</a>
+                            <a href={article.url} target="_blank" rel="noreferrer">Read more...</a>
                         </div>
                     </div>
                 </a>
